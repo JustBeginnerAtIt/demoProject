0de9404057e4ff8244b4bc43ae1bd5b8b4ea3385
JustBeginnerAtIt <tarabukin99@icloud.com>
2025-07-17 03:14:33 +0800

    initial commit

--- src/main/java/com/project/demo/controller/ProductController.java
+++ src/main/java/com/project/demo/controller/ProductController.java
@@ -1,13 +1,21 @@
 package com.project.demo.controller;
 
-import com.project.demo.controller.payload.NewProductPayLoad;
 import com.project.demo.controller.payload.UpdateProductPayload;
 import com.project.demo.entity.Product;
 import com.project.demo.service.ProductService;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.MessageSource;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Locale;
+import java.util.NoSuchElementException;
 
 @Controller
 @RequiredArgsConstructor
@@ -16,9 +24,11 @@ public class ProductController {
 
     private final ProductService productService;
 
+    private final MessageSource messageSource;
+
     @ModelAttribute("product")
     public Product product(@PathVariable("productId") Integer productId) {
-        return this.productService.findProduct(productId).orElseThrow();
+        return this.productService.findProduct(productId).orElseThrow(() -> new NoSuchElementException("catalogue.errors.product.not_found"));
     }
 
     @GetMapping
@@ -32,9 +42,17 @@ public class ProductController {
     }
 
     @PostMapping("edit")
-    public String updateProduct(@ModelAttribute("product") Product product, UpdateProductPayload payload) {
-        this.productService.updateProduct(product.getId(), payload.title(), payload.details());
-        return "redirect:/catalogue/products/%d".formatted(product.getId());
+    public String updateProduct(@ModelAttribute(name = "product", binding = false) Product product, @Valid UpdateProductPayload payload,
+                                BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("payLoad", payload);
+            model.addAttribute("errors", bindingResult.getAllErrors().stream()
+                    .map(ObjectError::getDefaultMessage).toList());
+            return "catalogue/products/edit";
+        } else {
+            this.productService.updateProduct(product.getId(), payload.title(), payload.details());
+            return "redirect:/catalogue/products/%d".formatted(product.getId());
+        }
     }
 
     @PostMapping("delete")
@@ -43,4 +61,12 @@ public class ProductController {
         return "redirect:/catalogue/products/list";
     }
 
+    @ExceptionHandler(NoSuchElementException.class)
+    public String handleNoSuchElementException(NoSuchElementException exception, Model model,
+                                               HttpServletResponse response, Locale locale) {
+        response.setStatus(HttpStatus.NOT_FOUND.value());
+        model.addAttribute("error",
+                this.messageSource.getMessage(exception.getMessage(), new Object[0], exception.getMessage(), locale));
+        return "errors/404";
+    }
 }
--- src/main/java/com/project/demo/controller/ProductsController.java
+++ src/main/java/com/project/demo/controller/ProductsController.java
@@ -3,9 +3,12 @@ package com.project.demo.controller;
 import com.project.demo.controller.payload.NewProductPayLoad;
 import com.project.demo.entity.Product;
 import com.project.demo.service.ProductService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,9 +32,17 @@ public class ProductsController {
     }
 
     @PostMapping("create")
-    public String createProduct(NewProductPayLoad payLoad) {
-        Product product = this.productService.createProduct(payLoad.title(), payLoad.details());
-        return "redirect:/catalogue/products/%d".formatted(product.getId());
+    public String createProduct(@Valid NewProductPayLoad payLoad,
+                                BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("payLoad", payLoad);
+            model.addAttribute("errors", bindingResult.getAllErrors().stream()
+                    .map(ObjectError::getDefaultMessage).toList());
+            return "catalogue/products/new_product";
+        } else {
+            Product product = this.productService.createProduct(payLoad.title(), payLoad.details());
+            return "redirect:/catalogue/products/%d".formatted(product.getId());
+        }
     }
 
 }
